573b4b7d1d111760ee985fda59079e6834f8b1e9
Erik Gillespie <erik.gillespie@gmail.com>
2013-09-30 22:24:15 -0400

    Use the tab available to the context menu as the message target instead of querying for the active tab.

--- background.js
+++ background.js
@@ -1,17 +1,11 @@
-function jqInspect(info, tab) {
-	chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            inspectWith: "jQuery"
-        });
+function inspectWith(info, tab) {
+    chrome.tabs.sendMessage(tab.id, {
+        inspectWith: "jQuery"
     });
 }
 
 chrome.contextMenus.create({
 	"title": "Inspect Element with jQuery",
 	"contexts": ["all"],
-	"onclick": jqInspect
+	"onclick": inspectWith
 });
-
