f4eb821c8997b0bfab2759241c8cea0232191696
egillespie <erik.gillespie@gmail.com>
2013-10-02 10:22:22 -0400

    Attempting to use experimental console access to add a message.

--- captureClick.js
+++ captureClick.js
@@ -2,8 +2,4 @@ function clicky(event) {
     window.lastElementClicked = event.target;
 }
 
-window.yourMom = "alert('Your mom')";
-
-
 window.oncontextmenu = clicky;
-
--- inspectWith.js
+++ inspectWith.js
@@ -3,8 +3,14 @@ s.src = chrome.extension.getURL("captureClick.js");
 document.body.appendChild(s);
 
 var inspectWithJQuery = function() {
-//    console.log($(window.lastElementClicked));
-    console.log(window.yourMom);
+    alert('hi');
+    var s = document.createElement("script");
+    s.src = chrome.extension.getURL("logElement.js");
+    s.onload = function() {
+        this.parentNode.removeChild(this);
+    };
+    document.body.appendChild(s);
+    chrome.experimental.devtools.console.addMessage("Log", "$($0)");
 };
 
 chrome.extension.onMessage.addListener(function (message, sender, callback) {
